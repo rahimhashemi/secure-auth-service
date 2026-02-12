2259741f29b99868b89124c97c85920104c3a765
simapth <hashemi.rahim@gmail.com>
2026-02-12 16:51:45 +0330

    init repo

---
+++ src/test/java/com/simpath/app/AppApplicationTests.java
@@ -0,0 +1,15 @@
+package com.simpath.app;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+        Assertions.assertTrue(true);
+	}
+
+}
